e99b06ebd31b6a4f67b455c7355f7db2c4a1f1d6
madamski9 <maciek.adamski9@gmail.com>
2024-11-02 19:28:18 +0100

    dodano nawigacje

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react'; 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const MyApp = () => {
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
   const myIp = "192.168.0.22:5233"; 
-  const [message, setMessage] = useState('');
+  const [ message, setMessage ] = useState('');
+
+  const getApi = async () => {
+    try {
+      const response = await axios.get(`http://${myIp}/api/helloworld`);
+      console.log(response.data);
+      setMessage(response.data); 
+    } catch (error) {
+      console.error("Wystąpił błąd: ", error);
+    }
+  };
 
   useEffect(() => {
-    const getApi = async () => {
-      try {
-        const response = await axios.get(`http://${myIp}/api/helloworld`);
-        console.log(response.data);
-        setMessage(response.data); 
-      } catch (error) {
-        console.error("Wystąpił błąd: ", error);
-      }
-    };
-  
     getApi();
   }, []); 
 
@@ -24,7 +28,32 @@ const MyApp = () => {
     <View style={styles.container}>
       <Text>Dane z backendu:</Text>
       <Text>{message}</Text> 
+      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+        <Text>Przejdź do szczegółów</Text>
+      </TouchableOpacity>
     </View>
+  );
+}
+
+const DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text>Wróć do strony głównej</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const MyApp = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
