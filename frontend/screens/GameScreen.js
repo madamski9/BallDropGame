eabc6ad4fb96009d1f75c06eb4399b4e0abe34ad
madamski9 <maciek.adamski9@gmail.com>
2024-11-03 21:04:03 +0100

    dodano kulke ktora reaguje na sensory

--- frontend/screens/GameScreen.js
+++ frontend/screens/GameScreen.js
@@ -1,23 +1,50 @@
-import React from 'react'; 
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react'; 
+import { StyleSheet, Text, View, TouchableOpacity, Animated, Dimensions } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 const GameScreen = ({ navigation }) => {
+    const [ ballPosition, setBallPosition ] = useState(new Animated.ValueXY({ x: 0, y: 0 }));   
+    const gravity = 3; // stala wartosc grawitacji
+
+    useEffect(() => {
+        const subscription = Accelerometer.addListener(({ x, y, z }) => {
+            let newX = ballPosition.x._value + x * 10;
+            let newY = ballPosition.y._value - y * 10 + gravity;
+
+            if (newX < -173) newX = -173;
+            if (newX > 173) newX = 173; 
+            if (newY > 386) newY = 386;
+            if (newY < -386) newY = -386; 
+                    
+            ballPosition.setValue({ x: newX, y: newY });
+        });
+      
+        return () => subscription.remove();
+    }, [ ballPosition ]);
+
     return (
-      <View style={styles.container}>
-        <Text>Details Screen</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-          <Text>Wróć do strony głównej</Text>
+        <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+            <Text>Wróć do strony głównej</Text>
         </TouchableOpacity>
-      </View>
+        <Animated.View style={[styles.ball, { transform: ballPosition.getTranslateTransform() }]} />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ball: {
+        width: 25,
+        height: 25,
+        backgroundColor: 'black',
+        borderRadius: 25,
+        position: 'absolute',
     },
 });
 
