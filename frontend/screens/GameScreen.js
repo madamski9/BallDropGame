65aa5bff1ce9f96a5c5e86d38459c1f6e45ef337
madamski9 <maciek.adamski9@gmail.com>
2024-11-03 21:29:17 +0100

    proba1 dodania obstacle

--- frontend/screens/GameScreen.js
+++ frontend/screens/GameScreen.js
@@ -5,6 +5,18 @@ import { Accelerometer } from 'expo-sensors';
 const GameScreen = ({ navigation }) => {
     const [ ballPosition, setBallPosition ] = useState(new Animated.ValueXY({ x: 0, y: 0 }));   
     const gravity = 3; // stala wartosc grawitacji
+    const [ obstacles, setObstacles ] = useState([
+        { x: 50, y: 500, width: 200, height: 10 }
+    ]);
+
+    const checkCollision = (ballX, ballY, obstacle) => {        
+        return (
+          ballX < obstacle.x + obstacle.width &&
+          ballX + 25 > obstacle.x && 
+          ballY < obstacle.y + obstacle.height &&
+          ballY + 25 > obstacle.y 
+        );
+    };
 
     useEffect(() => {
         const subscription = Accelerometer.addListener(({ x, y, z }) => {
@@ -15,7 +27,15 @@ const GameScreen = ({ navigation }) => {
             if (newX > 173) newX = 173; 
             if (newY > 386) newY = 386;
             if (newY < -386) newY = -386; 
-                    
+            
+            for (let obstacle of obstacles) {
+                if (checkCollision(newX, newY, obstacle)) {
+                  newX = ballPosition.x._value;
+                  newY = ballPosition.y._value;
+                  break;
+                }
+            }
+
             ballPosition.setValue({ x: newX, y: newY });
         });
       
@@ -24,10 +44,24 @@ const GameScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
             <Text>Wróć do strony głównej</Text>
         </TouchableOpacity>
         <Animated.View style={[styles.ball, { transform: ballPosition.getTranslateTransform() }]} />
+        {obstacles.map((obstacle, index) => (
+        <View
+            key={index}
+            style={[
+                styles.obstacle,
+                {
+                left: obstacle.x,
+                top: obstacle.y,
+                width: obstacle.width,
+                height: obstacle.height,
+                },
+            ]}
+            />
+        ))}
         </View>
     );
 }
@@ -46,6 +80,10 @@ const styles = StyleSheet.create({
         borderRadius: 25,
         position: 'absolute',
     },
+    obstacle: {
+        backgroundColor: 'black',
+        position: 'absolute',
+    }
 });
 
 export default GameScreen;
